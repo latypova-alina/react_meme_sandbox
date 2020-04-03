db215c74c6c68624dd4e7badf5556b7a06284185
Alina Latypova <alina.latypova@flatstack.com>
2020-04-03 14:49:08 +0200

    add useEffect

--- src/components/Memes/Memes.js
+++ src/components/Memes/Memes.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Meme from './Meme/Meme';
 
 const memes = (props) => props.collection.cats.map(cat => {
+  useEffect(() => {
+    console.log("Memes.js useEffect");
+
+    //здесь мог быть ваш эйчтитипи реквест
+    setTimeout(() => {
+      alert('Saved fake data');
+    }, 1000);
+
+    return () => {
+      console.log("Memes.js cleanup work in useEffect");
+    };
+  }, [cat.id]);
+
   return (
     <Meme
       image_source={cat.image}
